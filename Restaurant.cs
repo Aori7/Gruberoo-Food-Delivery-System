dcec6fbe349000df6c6d581c3a493cd4f3456e3b
Adawiyah <n.adawiyahisham@gmail.com>
2026-02-02 01:01:23 +0800

    set up classes files

---
+++ Restaurant.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG2_ASG_Gruberoo_Del_System
+{
+    internal class Restaurant
+    {
+    }
+}
