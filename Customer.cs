02867e1c819804074a658a417592c95af226196c
Adawiyah <n.adawiyahisham@gmail.com>
2026-02-03 21:21:34 +0800

    implement customer class (no ref yet)

--- Customer.cs
+++ Customer.cs
@@ -6,7 +6,57 @@ using System.Threading.Tasks;
 
 namespace PRG2_ASG_Gruberoo_Del_System
 {
-    internal class Customer
+    class Customer
     {
+        //att
+        private string emailAddress;
+        private string customerName;
+        //ref
+
+        // properties
+        public string EmailAddress
+        {
+            get {  return emailAddress; }
+            set { emailAddress = value; }
+        }
+        public string CustomerName
+        {
+            get { return customerName; }
+            set { customerName = value; }
+        }
+        //ref
+
+
+        // constructor
+        // default
+        public Customer() { }
+
+        // parameterized constructor
+        public Customer(string email, string name)
+        {
+            emailAddress = email;
+            customerName = name;
+        }
+        //other methods
+        public void AddOrder()
+        {
+
+        }
+        public void DisplayAllOrders()
+        {
+
+        }
+        public bool RemoveOrder()
+        {
+
+        }
+        // to string
+        public override string ToString()
+        {
+            return "Customer Email: " + emailAddress
+                + "\n"
+                + "Customer Name: " + customerName + "\n";
+        }
+
     }
 }
