fd800bf41c5e9fb271fdbb242148d413049a2ad5
Adawiyah <n.adawiyahisham@gmail.com>
2026-02-03 23:47:29 +0800

    program template

--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
-﻿// student number: S10275270A
-// Student name: Adawiyah
-// Partner Name: Rui Min
+﻿// Student Number:
+// Student Name: 
+// Partner Name: 
+
+using PRG2_ASG_Gruberoo_Del_System;
+
+// =============== basic features ===============
+// student 1: ada [TODO: 2,3,5,7 ]
+// student 2: rui min [TODO: 1,4,6,8 ]
+
+// todo 1: load files (restaurants and food items)
+// todo 2: load files (customers and orders)
+// todo 3: list all restaurants and menu items
+// todo 4: list all orders with basic information
+// todo 5: create a new order
+// todo 6: prompt an order
+// todo 7: modify an existing order
+// todo 8: delete an existing order
+
+// ==============================================
