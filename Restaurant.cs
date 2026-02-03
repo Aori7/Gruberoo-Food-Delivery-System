d2283abc0384573b8b10c15fc30cc2fdb11e31cf
Adawiyah <n.adawiyahisham@gmail.com>
2026-02-03 21:16:30 +0800

    continue implementing restaurant class

--- Restaurant.cs
+++ Restaurant.cs
@@ -14,6 +14,7 @@ namespace PRG2_ASG_Gruberoo_Del_System
         private string restaurantEmail;
         // ref
         private List<Menu> menus;
+        private List<Order> orders;
 
         // properties
         public string RestaurantId
@@ -37,11 +38,17 @@ namespace PRG2_ASG_Gruberoo_Del_System
             get { return menus; }
             set { menus = value; }
         }
+        public List<Order> Orders
+        {
+            get { return orders; }
+            set {  orders = value; }
+        }
 
         // default constructors
         public Restaurant()
         {
             menus = new List<Menu>();
+            orders = new List<Order>();
         }
         // parameterized constructor
         public Restaurant(string rid, string rname, string remail)
@@ -50,20 +57,35 @@ namespace PRG2_ASG_Gruberoo_Del_System
             restaurantName = rname;
             restaurantEmail = remail;
             menus = new List<Menu>();
+            orders = new List<Order>();
         }
 
         // other methods
         public void DisplayOrders()
         {
-
+            foreach (Order order in orders)
+            {
+                Console.WriteLine(order);
+            }
         }
         public void ShowSpecialOffers()
         {
-
+            foreach(Order order in orders)
+            {
+                // check if the order has a special offer - if not null
+                if (order.SpecialOffer != null)
+                {
+                    Console.WriteLine(order.SpecialOffer);
+                }
+            }
         }
         public void DisplayMenu()
         {
-
+            foreach (Menu menu in menus) 
+            {
+                Console.WriteLine(menu);
+                menu.DisplayFoodItems();
+            }
         }
         public void AddMenu(Menu menu)
         {
